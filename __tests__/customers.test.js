c0553004e3aff9886c7bd4475325d7d2040f7fae
Concross <cacrossley22@gmail.com>
2018-09-20 15:20:34 -0700

    Customer class constructor complete

--- __tests__/customers.test.js
+++ __tests__/customers.test.js
@@ -3,16 +3,63 @@
 import Customers from '../src/lib/customers';
 
 describe('Customers constructor tests', () => {
-  test('that constructor instantiates an object of type Customers', () => {
-    let actual = new Customers();
+  let customer = new Customers();
 
-    expect(actual).toBeInstanceOf(Customers);
+  test('that constructor instantiates an object of type Customers', () => {
+
+    expect(customer).toBeInstanceOf(Customers);
   });
 
   test('that a new customer object has a defined id', () => {
-    let customer = new Customers();
     let actual = customer.id;
 
     expect(actual).toBeDefined();
+  });
+
+  test('that a new customer object has a an empty array of orders', () => {
+    let actual = customer.orderHistory;
+    let expected = [];
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('that a new customer object has an age if given or null otherwise', () => {
+    expect(customer.age).toBeNull();
+
+    let oldCustomer = new Customers(99);
+    let expected = 99;
+    expect(oldCustomer.age).toBe(expected);
+  });
+
+  test('that a new customer object has a gender if given or null otherwise', () => {
+    expect(customer.gender).toBeNull();
+
+    let genderCustomer = new Customers(99, 'male');
+    let expected = 'male';
+    expect(genderCustomer.gender).toBe(expected);
+  });
+
+  test('that a new customer object has an ethnicity if given or null otherwise', () => {
+    expect(customer.ethnicity).toBeNull();
+
+    let ethnicCustomer = new Customers(99, 'male', 'hispanic');
+    let expected = 'hispanic';
+    expect(ethnicCustomer.ethnicity).toBe(expected);
+  });
+
+  test('that a new customer object has a salary if given or 0 otherwise', () => {
+    expect(customer.salary).toBe(0);
+
+    let salariedCustomer = new Customers(99, 'male', 'hispanic', 100000);
+    let expected = 100000;
+    expect(salariedCustomer.salary).toBe(expected);
+  });
+
+  test('that a new customer object has a maritalStatus if given or null otherwise', () => {
+    expect(customer.maritalStatus).toBeNull();
+
+    let marriedCustomer = new Customers(99, 'male', 'hispanic', 100000, 'married');
+    let expected = 'married';
+    expect(marriedCustomer.maritalStatus).toBe(expected);
   });
 });
--- src/lib/customers.js
+++ src/lib/customers.js
@@ -3,9 +3,14 @@
 import uuid from 'uuid/v1';
 
 class Customers {
-  constructor() {
+  constructor(age = null, gender = null, ethnicity = null, salary = 0, maritalStatus = null) {
     this.id = uuid();
-
+    this.orderHistory = [];
+    this.age = age;
+    this.gender = gender;
+    this.ethnicity = ethnicity;
+    this.salary = salary;
+    this.maritalStatus = maritalStatus;
   }
 }
 
